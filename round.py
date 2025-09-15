e03227dd968d4b37a038f14180e8924918f3d8b2
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2025-09-14 21:55:40 -0400

    Restructured game so new round objects generate and pass game data on

--- round.py
+++ round.py
@@ -6,49 +6,30 @@ from AIPlayer import AIPlayer
 import utils
 
 class round:
-    def __init__(self, buyIn : int, minInitBet : int, playerList : list):
+    def __init__(self, minInitBet : int, playerList : list):
+
+        previousBet = 0
         """This constructor will initilize the game by creating a deck and shuffling the cards"""
 
+        self.playerList = playerList
         self.pot = 0 # Before betting begins, the pot is set to 0
-
         self.deck = Deck() # Creating the Deck object that belongs to the game class
         self.deck.shuffleCards() # Shuffling all the cards in said deck
 
-        userName = input("What is your name?: ")
-        money = input("How much money do you have?: ")
-
-        money = utils.inputValidation(money, int) # Validating that the input is an integer keeps reprompting till the input is valid
-
-        print(f"Hello, your name is {userName} and you are starting out with ${money}, goodluck!") # Delete this later
-
-        self.playerList.append(Player(userName, money)) # For texting purposes the player will start as the first player/small blind
-
-        self.playerList = createAIPlayers(self.playerList) # Creating 3 AI players and appending them to the playerList
-
-        self.playerList = kick(self.playerList)
-
-        print("The following players are in the game:") # Mostly for testing purposes but this introduces the players
-        for i in range(len(self.playerList)):
-            print(f"{self.playerList[i].getName()} has a chipstack of {self.playerList[i].getChipStack()}")
-
-        self.playerList = drawCards(self.playerList, self.deck) # Drawing 2 cards for each player and appending them to their hand
+        drawCards(self.playerList, self.deck) # Drawing 2 cards for each player and appending them to their hand
         
         print("Your hand is: ")
-        self.playerList[0].hand.printHand() # Printing the users hand to the terminal for testing purposes
+        playerList[0].hand.printHand() # Printing the users hand to the terminal for testing purposes
 
         print("Starting round")
 
         # Clarifying who is small and big blind
         print(self.playerList[0].getName() + " is the small blind")
         print(self.playerList[1].getName() + " is the big blind")
-        print(self.playerList[2].getName() + " is a regular player")
-        print(self.playerList[3].getName() + " is a regular player")
+        for i in range(2, len(self.playerList)):
+            print(self.playerList[i].getName() + " is a regular player")
 
-        attemptedBet = 0
         # Start of preflop betting
-
-
-
 
         # This logic only runs if the first player is the is the user, otherwise AI logic will be used
         if not self.playerList[0].getIsAIPlayer():  # If the small blind is the user
@@ -63,28 +44,10 @@ class round:
                 attemptedBet = utils.inputValidation(
                 input(f"Your minimum bet is {minInitBet//2}, how much would you like to bet?: "), int
                 )
-        # If the first player is the AI, the follwing code runs
+        # If the first player is the AI, the following code runs
         # else:
-            
-            
-
-        
-
-        
-
-
-
 
         self.playerList[0].setBet(minInitBet // 2) # Small blind posts half the minimum bet
-
-
-
-
-        
-
-
-
-        
 
         # # Positions on the table will be based on the index of the player in the playerList list (e.g index 0 = position 1)
         
@@ -115,13 +78,11 @@ def newRoundRotation(list: list) -> list:
     list.insert(0, P4)
     return list
 
-def drawCards(playerList: list, deck : Deck) -> list:
-    """Will draw 2 cards per player and will append those to each players hand"""
+def drawCards(playerList: list, deck : Deck) -> None:
+    """Will draw 2 cards per player and will append those to each players hand, no return value since the playerList is passed by reference"""
     for i in range(len(playerList)): # For each player
         playerList[i].hand.acceptCard(deck.draw())
         playerList[i].hand.acceptCard(deck.draw())
-
-    return playerList
 
 def kick(playerList: list) -> list:
     """Remove players with 0 chips from the game by going though the player list and checking their chipstack.
@@ -151,25 +112,34 @@ def main():
     userName = input("What is your name?: ")
     money = input("How much money do you have?: ")
 
-    money = utils.inputValidation(money, int, "+") # Validating that the input is an integer keeps reprompting till the input is valid
+    money = utils.inputValidation(money, int, "+") # Validating that the input is an integer and is of proper sign, keeps reprompting till the input is valid
 
-    print(f"Hello, your name is {userName} and you are starting out with ${money}, goodluck!") # Delete this later
-
-    #playerList.append(Player(userName, money)) # For texting purposes the player will start as the first player/small blind
-
-    
-    print("This is round " + str(roundCounter))
-
-    # Validating that the input is an integer and is positive
-    while(True):
-        numAIPlayers = utils.inputValidation(input("How many players would you like to play against?: "), int)
-        if(numAIPlayers > 0):
-            break
-        else:
-            print("Invalid input, cant have negative")
+    print(f"Hello, your name is {userName} and you are starting out with ${money}, goodluck!")
 
     # Initializing the list that will hold all players in the game
     playerList = []
+
+    playerList.append(Player(userName, money)) # For simplicity/testing purposes the player will start as the first player/small blind
+
+    # Validating and getting the number of players from the AI
+    numAIPlayers = utils.inputValidation(input("How many players would you like to play against?: "), int, "+")
+
+    # For that will create the AI players and add them to the player list (default money will generate between 200 and 600 and default aggresion is 5)
+    for i in range(numAIPlayers):
+        playerList.append(AIPlayer(f"Bot {i+1}", utils.genRandNum(200, 600), 5))
+        print(playerList[i+1].getName() + f" has joined the game with ${playerList[i+1].getChipStack()}")
+
+
+    while(True):
+        playerList = kick(playerList)
+        if len(playerList) < 2:
+            print(f"{playerList[0].getName()} is the last player remaining and wins the game!")
+            break
+        print(f"This is round {roundCounter}, lets begin !!!!")
+        
+        newRound = round(5, playerList)
+
+
 
     # Creating the AIplayers and appending them to the playerList
     playerList = utils.inputValidation(numAIPlayers, int)
